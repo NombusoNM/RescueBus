54a64a2450f9e50be57360d0b59b72b0af709ad0
Nombuso Mdletshe <nombusomnmdletshe@outlook.com>
2025-05-19 20:15:01 +0200

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace u17164070_HW01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u17164070_HW01.MvcApplication" Language="C#" %>
